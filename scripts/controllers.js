3c86acd0062ae762f94ee70e83d5c02133e1896b
Melody Truong <mtruong@groupon.com>
2016-09-18 16:41:19 -0700

    Add Clinic Deletion

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -2,13 +2,27 @@
 
 angular.module('aloeApp', [])
   .controller('ClinicsController', ['$scope', '$http', function($scope, $http) {
-    var clinicsResponse = $http.get('http://localhost:8000/clinics');
+    $scope.fetchClinics = function () {
+      var clinicsResponse = $http.get('http://localhost:8000/clinics');
+      clinicsResponse.success(function (data, status, headers, config) {
+        $scope.clinics = data;
+        $('#clinics').fadeTo('fast', 1);
+      });
+      clinicsResponse.error(function (data, status, headers, config) {
+        console.log("Fetch request for clinics failed!");
+      });
+    }
 
-    clinicsResponse.success(function (data, status, headers, config) {
-      $scope.clinics = data;
-    });
+    $scope.deleteClinic = function ($index, clinic) {
+      var clinicsResponse = $http.delete('http://localhost:8000/clinics/' + clinic.id);
+      clinicsResponse.success(function (data, status, headers, config) {
+        console.log('Clinic successfully deleted!');
+        $scope.clinics.splice($index, 1);
+      });
+      clinicsResponse.error(function (data, status, headers, config) {
+        console.log('Error deleting clinic!');
+      });
+    };
 
-    clinicsResponse.error(function (data, status, headers, config) {
-      console.log("Fetch request for clinics failed!");
-    });
+    $scope.fetchClinics();
   }]);
